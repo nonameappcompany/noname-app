01ddebaa3b9479f23619541289bba5b0484f2682
andrvalo <avanegas@pratechgroup.com>
2020-04-17 17:19:33 -0500

    cambio express

---
+++ dist/precache-manifest.038e1b26dfe8f5889d39d910baaaf25d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455681b02d5c5e0d5de8",
+    "url": "/css/app.2e5b9c1d.css"
+  },
+  {
+    "revision": "7c629cfbb5e2d1917198",
+    "url": "/css/chunk-vendors.0bfa3741.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "035dc0c4a74bbabe32fc4527b3365d15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7d33c656d2e4fa904aa",
+    "url": "/js/about.04629bd4.js"
+  },
+  {
+    "revision": "455681b02d5c5e0d5de8",
+    "url": "/js/app.8f8390e8.js"
+  },
+  {
+    "revision": "7c629cfbb5e2d1917198",
+    "url": "/js/chunk-vendors.c025f1a2.js"
+  },
+  {
+    "revision": "d8e1046c69f35ece0df3fc1339d79ce6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
